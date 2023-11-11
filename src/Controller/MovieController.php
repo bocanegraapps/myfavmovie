5a078e00acc6f44a392ac30a24c92e6575a54239
bocanegraapps <informaticabocanegra@gmail.com>
2023-11-11 01:01:53 +0100

    initial 11-11

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Entity\User;
+use App\Event\CommentCreatedEvent;
+use App\Form\CommentType;
+use App\Repository\PostRepository;
+use App\Repository\TagRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+/**
+ * Controller used to manage blog contents in the public part of the site.
+ *
+ * @author Ryan Weaver <weaverryan@gmail.com>
+ * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ */
+#[Route('/myMovies')]
+final class MovieController extends AbstractController
+{
+    /**
+     * NOTE: For standard formats, Symfony will also automatically choose the best
+     * Content-Type header for the response.
+     *
+     * See https://symfony.com/doc/current/routing.html#special-parameters
+     */
+    #[Route('/main_app_index', name: 'main_app_index', defaults: ['page' => '1', '_format' => 'html'], methods: ['GET'])]
+    #[Cache(smaxage: 10)]
+
+    public function index(Request $request, int $page, string $_format, PostRepository $posts, TagRepository $tags): Response
+    {
+        $tag = null;
+        if ($request->query->has('tag')) {
+            $tag = $tags->findOneBy(['name' => $request->query->get('tag')]);
+        }
+        $latestPosts = $posts->findLatest($page, $tag);
+
+        // Every template name also has two extensions that specify the format and
+        // engine for that template.
+        // See https://symfony.com/doc/current/templates.html#template-naming
+        return $this->render('app/index.'.$_format.'.twig', [
+            'paginator' => $latestPosts,
+            'tagName' => $tag?->getName(),
+        ]);
+    }
+
+    
+}
